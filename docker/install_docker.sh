e744182ec3ee57bcbc970b089201520752df98a9
Diego Ordoñez <dordonez4@gmail.com>
2025-04-25 12:59:41 +0200

    Add Docker installation script for Ubuntu

---
+++ docker/install_docker.sh
@@ -0,0 +1,22 @@
+# Install Docker if not installed
+if ! command -v docker &>/dev/null; then
+    echo "Installing Docker..."
+    sudo apt-get update
+    sudo apt-get install ca-certificates curl
+    sudo install -m 0755 -d /etc/apt/keyrings
+    sudo curl -fsSL https://download.docker.com/linux/ubuntu/gpg -o /etc/apt/keyrings/docker.asc
+    sudo chmod a+r /etc/apt/keyrings/docker.asc
+
+    # Add the repository to Apt sources:
+    echo \
+    "deb [arch=$(dpkg --print-architecture) signed-by=/etc/apt/keyrings/docker.asc] https://download.docker.com/linux/ubuntu \
+    $(. /etc/os-release && echo "${UBUNTU_CODENAME:-$VERSION_CODENAME}") stable" | \
+    sudo tee /etc/apt/sources.list.d/docker.list > /dev/null
+    sudo apt-get update
+    sudo apt-get install -y docker-ce docker-ce-cli containerd.io docker-buildx-plugin docker-compose-plugin
+    echo "Docker installed succesfully. Landing test contantainer..."
+    sudo docker run hello-world
+    sudo usermod -aG docker $USER
+else
+    echo "Docker is already installed: Version:$(docker --version)"
+fi
